7b146822b6ef5d85dd6a95c7ee602a882101d94b
Md. Meshbaul Haque Tamim <2102028@che.buet.ac.bd>
2026-02-21 19:48:37 +0600

    feat: Add initial environment variables for application configuration including database, authentication, and cloud service credentials.

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -3,23 +3,21 @@ import { DB_NAME } from "../constants/index.js";
 
 const connectDB = async () => {
   mongoose.connection.on("connected", () => {
-    console.log(
-      `\n✅ MongoDB connected !! DB Host: ${mongoose.connection.host}`
-    );
+    console.log(`\nMongoDB connected !! DB Host: ${mongoose.connection.host}`);
   });
 
   mongoose.connection.on("error", (err) => {
-    console.error(`❌ MongoDB runtime connection error: ${err}`);
+    console.error(`MongoDB runtime connection error: ${err}`);
   });
 
   mongoose.connection.on("disconnected", () => {
-    console.log("❕ MongoDB connection lost. Attempting to reconnect...");
+    console.log("MongoDB connection lost. Attempting to reconnect...");
   });
 
   try {
     await mongoose.connect(`${process.env.DB_URL}/${DB_NAME}`);
   } catch (error) {
-    console.error(`\n❌ Initial MongoDB connection FAILED: ${error.message}`);
+    console.error(`\nInitial MongoDB connection FAILED: ${error.message}`);
     process.exit(1);
   }
 };
